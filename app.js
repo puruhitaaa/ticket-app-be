bcae23b32ae34ed1ef859f328f4713fd1a257936
baiqueee <hughdev101@gmail.com>
2024-03-03 23:46:49 +0700

    [+-] fix maybe?

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+
 const app = express();
 const port = 3000;
+
 const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // support json encoded bodies
